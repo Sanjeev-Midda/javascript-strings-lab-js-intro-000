bc39aea2dec44a85fb7097f36303db1311a15d7f
epic-store-5589 <s.midda1@gmail.com>
2020-04-25 14:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Hello, " + "Neil deGrasse Tyson"
+var specialGuest = greeting + "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
